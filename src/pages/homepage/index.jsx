93b10d2c1206e212c6782975f03382342b30fab3
sontakmtp-cell <sontak@gmail.com>
2025-09-11 15:49:23 +0700

    update previews

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -16,6 +16,10 @@ const Homepage = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  const siteUrl = 'https://kythuatvang.com';
+  const pageUrl = `${siteUrl}/homepage`;
+  const shareImage = `${siteUrl}/assets/images/social-share-default.png`;
+
   return (
     <>
       <Helmet>
@@ -25,9 +29,24 @@ const Homepage = () => {
           content="Kết nối chuyên gia kỹ thuật với dự án của bạn. Tìm kiếm freelancer, tuyển dụng nhân tài, mua sắm sản phẩm kỹ thuật và sử dụng công cụ tính toán chuyên ngành." 
         />
         <meta name="keywords" content="freelancer kỹ thuật, tuyển dụng kỹ sư, sản phẩm kỹ thuật, công cụ tính toán, AutoCAD, Revit, thiết kế cơ khí" />
+
+        {/* Open Graph */}
         <meta property="og:title" content="TechMarketplace Pro - Nền tảng kỹ thuật hàng đầu Việt Nam" />
         <meta property="og:description" content="Kết nối chuyên gia kỹ thuật với dự án của bạn. Hơn 50,000 chuyên gia và 12,847 dự án hoàn thành." />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:image" content={shareImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="TechMarketplace Pro" />
+        <meta property="og:locale" content="vi_VN" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="TechMarketplace Pro - Nền tảng kỹ thuật hàng đầu Việt Nam" />
+        <meta name="twitter:description" content="Kết nối chuyên gia kỹ thuật với dự án của bạn. Hơn 50,000 chuyên gia và 12,847 dự án hoàn thành." />
+        <meta name="twitter:image" content={shareImage} />
+
         <link rel="canonical" href="/homepage" />
       </Helmet>
 
@@ -86,7 +105,7 @@ const Homepage = () => {
                 </div>
                 <div className="flex items-center space-x-2">
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" />
                   </svg>
                   <span className="text-sm">Bảo mật 100%</span>
                 </div>
